e11628eb567544bf75506f8ec782ee9be836817f
Westley Feller <feller.westley@gmail.com>
2019-12-15 18:56:06 -0800

    Fetching colors from server

--- client/src/api/axiosWithAuth.js
+++ client/src/api/axiosWithAuth.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
 
-const axiosWithAuth = () => {
-  const token = localStorage.getItem('token');
-
-  return axios.create({
-    baseURL: 'http://localhost:5000/',
+const axiosWithAuth = () =>
+  axios.create({
+    baseURL: 'http://localhost:5000',
     headers: {
-      Authorization: token,
+      Authorization: localStorage.getItem('token'),
     },
   });
-};
 
 export default axiosWithAuth;
--- client/src/components/BubblePage.jsx
+++ client/src/components/BubblePage.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Bubbles from './Bubbles';
 import ColorList from './ColorList';
+import axiosWithAuth from '../api/axiosWithAuth';
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() => {
+    axiosWithAuth()
+      .get('/api/colors')
+      .then((response) => {
+        setColorList(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <>
